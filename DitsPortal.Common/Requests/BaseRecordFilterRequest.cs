0bd1d9aa89f128d33701dad117ab445bac7d75c5
Saddam Husain <saddamhusaainwins@gmail.com>
2020-03-05 15:51:55 +0530

    changes

--- DitsPortal.Common/Requests/BaseRecordFilterRequest.cs
+++ DitsPortal.Common/Requests/BaseRecordFilterRequest.cs
@@ -7,7 +7,7 @@ namespace DitsPortal.Common.Requests
    public class BaseRecordFilterRequest
     {
         public int Page { get; set; } = 1;
-        public int Limit { get; set; } = 10; 
+        public int Limit { get; set; } = 10;  
         public string OrderBy { get; set; } = "CreatedOn";
         public bool OrderByDescending { get; set; } = true;
         public bool AllRecords { get; set; } = false;
